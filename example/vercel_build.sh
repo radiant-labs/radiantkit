62142d23c768814aabd6fa5c71d11e48bd452e1b
Nikhil Gupta <inbox.guptanikhil@gmail.com>
2023-08-24 18:34:05 +0530

    Adds vercel build script

---
+++ example/vercel_build.sh
@@ -0,0 +1,9 @@
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+source "$HOME/.cargo/env"
+
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+
+yarn build:wasm
+
+yarn build
