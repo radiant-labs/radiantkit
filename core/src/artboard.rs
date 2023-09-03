8d94d774cecd646b407a95652cf254ea632115b9
Nikhil Gupta <reach.guptanikhil@gmail.com>
2023-09-03 16:39:21 +0530

    Adds ability to render a rectangle

---
+++ core/src/artboard.rs
@@ -0,0 +1,26 @@
+use super::{RadiantNode, RadiantNodeRenderable};
+use std::sync::Arc;
+
+pub struct RadiantArtboardNode {
+    pub nodes: Vec<Arc<dyn RadiantNode>>,
+}
+
+impl RadiantArtboardNode {
+    pub fn new() -> Self {
+        Self { nodes: vec![] }
+    }
+
+    pub fn add(&mut self, node: Arc<dyn RadiantNode>) {
+        self.nodes.push(node);
+    }
+}
+
+impl RadiantNodeRenderable for RadiantArtboardNode {
+    fn render<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
+        println!("Rendering artboard");
+        // self.nodes.iter().for_each(|node| node.render(render_pass));
+        for node in &self.nodes {
+            node.render(render_pass);
+        }
+    }
+}
