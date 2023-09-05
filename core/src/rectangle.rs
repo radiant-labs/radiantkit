3d5872dfc5253f23c00d3226c6b1ec86a010f282
Nikhil Gupta <nikhil@getnyra.ai>
2023-09-05 16:30:16 +0530

    Adds ability to render offscreen

--- core/src/rectangle.rs
+++ core/src/rectangle.rs
@@ -75,6 +75,7 @@ const INDICES: &[u16] = &[0, 1, 2, 2, 3, 0];
 pub struct RadiantRectangleNode {
     pub position: [f32; 2],
     render_pipeline: Arc<wgpu::RenderPipeline>,
+    offscreen_render_pipeline: Arc<wgpu::RenderPipeline>,
     vertex_buffer: wgpu::Buffer,
     index_buffer: wgpu::Buffer,
     num_indices: u32,
@@ -182,9 +183,52 @@ impl RadiantRectangleNode {
             })
             .into();
 
+        let offscreen_render_pipeline = device
+            .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main",     // 1.
+                    buffers: &[Vertex::desc()], // 2.
+                },
+                fragment: Some(wgpu::FragmentState {
+                    // 3.
+                    module: &shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        // 4.
+                        format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw, // 2.
+                    cull_mode: Some(wgpu::Face::Back),
+                    // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    // Requires Features::DEPTH_CLIP_CONTROL
+                    unclipped_depth: false,
+                    // Requires Features::CONSERVATIVE_RASTERIZATION
+                    conservative: false,
+                },
+                depth_stencil: None, // 1.
+                multisample: wgpu::MultisampleState {
+                    count: 1,                         // 2.
+                    mask: !0,                         // 3.
+                    alpha_to_coverage_enabled: false, // 4.
+                },
+                multiview: None, // 5.
+            })
+            .into();
+
         Self {
             position,
             render_pipeline,
+            offscreen_render_pipeline,
             vertex_buffer,
             index_buffer,
             num_indices,
@@ -194,10 +238,14 @@ impl RadiantRectangleNode {
 }
 
 impl RadiantNodeRenderable for RadiantRectangleNode {
-    fn render<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
+    fn render<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>, offscreen: bool) {
         log::debug!("Rendering rectangle");
 
-        render_pass.set_pipeline(&self.render_pipeline); // 2.
+        render_pass.set_pipeline(if offscreen {
+            &self.offscreen_render_pipeline
+        } else {
+            &self.render_pipeline
+        }); // 2.
         render_pass.set_bind_group(0, &self.model_bind_group, &[]);
         render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
         render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
--- main/src/renderer.rs
+++ main/src/renderer.rs
@@ -134,7 +134,7 @@ impl RenderState {
                 depth_stencil_attachment: None,
             });
 
-            document.render(&mut render_pass);
+            document.render(&mut render_pass, false);
         }
 
         // submit will accept anything that implements IntoIter
@@ -142,5 +142,113 @@ impl RenderState {
         output.present();
 
         Ok(())
+    }
+
+    pub async fn select(&self, document: &RadiantDocumentNode) {
+        log::info!("Selecting...");
+        let texture_size = 256u32;
+
+        let texture_desc = wgpu::TextureDescriptor {
+            size: wgpu::Extent3d {
+                width: texture_size,
+                height: texture_size,
+                depth_or_array_layers: 1,
+            },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::COPY_SRC | wgpu::TextureUsages::RENDER_ATTACHMENT,
+            label: None,
+            view_formats: &[],
+        };
+        let texture = self.device.create_texture(&texture_desc);
+        let texture_view = texture.create_view(&Default::default());
+
+        // we need to store this for later
+        let u32_size = std::mem::size_of::<u32>() as u32;
+
+        let output_buffer_size = (u32_size * texture_size * texture_size) as wgpu::BufferAddress;
+        let output_buffer_desc = wgpu::BufferDescriptor {
+            size: output_buffer_size,
+            usage: wgpu::BufferUsages::COPY_DST
+                // this tells wpgu that we want to read this buffer from the cpu
+                | wgpu::BufferUsages::MAP_READ,
+            label: None,
+            mapped_at_creation: false,
+        };
+        let output_buffer = self.device.create_buffer(&output_buffer_desc);
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        {
+            let render_pass_desc = wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &texture_view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            };
+            let mut render_pass = encoder.begin_render_pass(&render_pass_desc);
+
+            document.render(&mut render_pass, true);
+        }
+
+        encoder.copy_texture_to_buffer(
+            wgpu::ImageCopyTexture {
+                aspect: wgpu::TextureAspect::All,
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            wgpu::ImageCopyBuffer {
+                buffer: &output_buffer,
+                layout: wgpu::ImageDataLayout {
+                    offset: 0,
+                    bytes_per_row: Some(u32_size * texture_size),
+                    rows_per_image: Some(texture_size),
+                },
+            },
+            texture_desc.size,
+        );
+
+        self.queue.submit(Some(encoder.finish()));
+
+        // We need to scope the mapping variables so that we can
+        // unmap the buffer
+        {
+            let buffer_slice = output_buffer.slice(..);
+
+            // NOTE: We have to create the mapping THEN device.poll() before await
+            // the future. Otherwise the application will freeze.
+            let (tx, rx) = futures_intrusive::channel::shared::oneshot_channel();
+            buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
+                tx.send(result).unwrap();
+            });
+            self.device.poll(wgpu::Maintain::Wait);
+            rx.receive().await.unwrap().unwrap();
+
+            let data = buffer_slice.get_mapped_range();
+
+            use image::{ImageBuffer, Rgba};
+            let buffer =
+                ImageBuffer::<Rgba<u8>, _>::from_raw(texture_size, texture_size, data).unwrap();
+            log::info!("Saving image.png");
+            #[cfg(not(target_arch = "wasm32"))]
+            buffer.save("image.png").unwrap();
+        }
+        output_buffer.unmap();
     }
 }
