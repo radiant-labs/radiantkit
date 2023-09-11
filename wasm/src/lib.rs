6ff53be379f463bfce15d27b9739ff5ea57b8dd9
Nikhil Gupta <reach.guptanikhil@gmail.com>
2023-09-11 18:22:19 +0530

    Adds ability to trigger responses to JS

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,14 +1,13 @@
 use std::sync::{Arc, RwLock};
 
-use radiant_main::{RadiantApp, RadiantMessage, RadiantTool};
+use radiant_main::{RadiantApp, RadiantMessage, RadiantTool, RadiantResponse};
 use wasm_bindgen::prelude::*;
 use winit::platform::web::EventLoopExtWebSys;
 use winit::{event_loop::EventLoop, window::WindowBuilder};
 
 #[wasm_bindgen(js_name = RadiantAppController)]
 pub struct RadiantAppController {
-    app: Arc<RwLock<RadiantApp>>,
-    js_callback: js_sys::Function,
+    app: Arc<RwLock<RadiantApp>>
 }
 
 #[wasm_bindgen(js_class = RadiantAppController)]
@@ -38,25 +37,30 @@ impl RadiantAppController {
             })
             .expect("Couldn't append canvas to document body.");
 
-        let app = Arc::new(RwLock::new(radiant_main::RadiantApp::new(window).await));
+        let f2 = f.clone();
+        let handler: Box<dyn Fn(RadiantResponse)> = Box::new(move |response: RadiantResponse| {
+            let this = JsValue::null();
+            let _ = f2.call1(&this, &serde_wasm_bindgen::to_value(&response).unwrap());
+        });
+
+        let app = Arc::new(RwLock::new(RadiantApp::new(window, handler).await));
         let weak_app = Arc::downgrade(&app);
 
-        let f2 = f.clone();
+        let f3 = f.clone();
 
         event_loop.spawn(move |event, _, control_flow| {
             if let Some(app) = weak_app.upgrade() {
                 if let Ok(mut app) = app.write() {
                     if let Some(response) = app.handle_event(event, control_flow) {
                         let this = JsValue::null();
-                        let _ = f2.call1(&this, &serde_wasm_bindgen::to_value(&response).unwrap());
+                        let _ = f3.call1(&this, &serde_wasm_bindgen::to_value(&response).unwrap());
                     }
                 }
             }
         });
 
         Self {
-            app,
-            js_callback: f.clone(),
+            app
         }
     }
 
