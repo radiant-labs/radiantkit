03bf1c0e691134690a7afdaf757dd6e5c85bd85c
Nikhil Gupta <inbox.guptanikhil@gmail.com>
2023-10-11 17:54:56 +0530

    UI alignment for canvas and panels

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,19 +1,39 @@
-import { RadiantToolbar } from "./components/RadiantToolbar";
-import { Stack } from "@mui/material";
-import { RadiantAppProvider } from "./contexts/RadiantAppContext";
-import { RadiantPropertiesPanel } from "./components/RadiantPropertiesPanel";
+import { RadiantToolbar } from './components/RadiantToolbar'
+import { Stack } from '@mui/material'
+import { RadiantAppProvider } from './contexts/RadiantAppContext'
+import { RadiantPropertiesPanel } from './components/RadiantPropertiesPanel'
 
 function App() {
-  return (
-    <RadiantAppProvider>
-      <div>
-        <Stack direction="row" spacing={2}>
-          <RadiantToolbar />
-          <RadiantPropertiesPanel />
-        </Stack>
-      </div>
-    </RadiantAppProvider>
-  );
+    return (
+        <RadiantAppProvider>
+            <div
+                id="overlay"
+                style={{
+                    position: 'absolute',
+                    zIndex: 1,
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    width: '100%',
+                    pointerEvents: 'none',
+                }}
+            >
+                <RadiantToolbar />
+                <RadiantPropertiesPanel />
+            </div>
+            <div
+                id="canvas-container"
+                style={{
+                    position: 'absolute',
+                    zIndex: 0,
+                    display: 'flex',
+                    alignItems: 'center',
+                    height: '100%',
+                    justifyContent: 'center',
+                    width: '100%',
+                }}
+            />
+        </RadiantAppProvider>
+    )
 }
 
-export default App;
+export default App
--- apps/web/src/components/RadiantColorPanel.tsx
+++ apps/web/src/components/RadiantColorPanel.tsx
@@ -1,77 +1,89 @@
-import { useContext, useEffect, useState } from "react";
-import { RadiantAppContext } from "../contexts/RadiantAppContext";
+import { useContext, useEffect, useState } from 'react'
+import { RadiantAppContext } from '../contexts/RadiantAppContext'
 
 function componentToHex(c: number) {
-    var hex = c.toString(16);
-    return hex.length === 1 ? "0" + hex : hex;
+    var hex = c.toString(16)
+    return hex.length === 1 ? '0' + hex : hex
 }
 
 function rgbToHex(r: number, g: number, b: number) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b)
 }
 
 function hexTorgba(hex: string) {
     if (hex.length === 4) {
-        hex = hex.replace(/([^#])/g, '$1$1');
+        hex = hex.replace(/([^#])/g, '$1$1')
     }
     return [
         parseInt(hex.substr(1, 2), 16),
         parseInt(hex.substr(3, 2), 16),
         parseInt(hex.substr(5, 2), 16),
-        255
-    ];
+        255,
+    ]
 }
 
 export function RadiantColorPanel() {
-    const { controller, response } = useContext(RadiantAppContext);
+    const { controller, response } = useContext(RadiantAppContext)
 
-    const [nodeId, setNodeId] = useState<number>(0); 
-    const [fillColor, setFillColor] = useState("#000000");
-    const [strokeColor, setStrokeColor] = useState("#000000");
+    const [nodeId, setNodeId] = useState<number>(0)
+    const [fillColor, setFillColor] = useState('#000000')
+    const [strokeColor, setStrokeColor] = useState('#000000')
 
     useEffect(() => {
         if (response?.NodeSelected) {
-            let node = response.NodeSelected.Rectangle;
-            setNodeId(node.id);
-            let { fill_color, stroke_color } = node.color;
-            setFillColor(rgbToHex(fill_color[0], fill_color[1], fill_color[2]));
-            setStrokeColor(rgbToHex(stroke_color[0], stroke_color[1], stroke_color[2]));
+            let node = response.NodeSelected.Rectangle
+            setNodeId(node.id)
+            let { fill_color, stroke_color } = node.color
+            setFillColor(rgbToHex(fill_color[0], fill_color[1], fill_color[2]))
+            setStrokeColor(
+                rgbToHex(stroke_color[0], stroke_color[1], stroke_color[2])
+            )
         }
-    }, [response]);
+    }, [response])
 
     useEffect(() => {
-        controller && controller.handleMessage({
-            SetFillColor: {
-                id: nodeId,
-                fill_color: hexTorgba(fillColor),
-            }
-        });
-    }, [controller, fillColor, nodeId]);
+        controller &&
+            controller.handleMessage({
+                SetFillColor: {
+                    id: nodeId,
+                    fill_color: hexTorgba(fillColor),
+                },
+            })
+    }, [controller, fillColor, nodeId])
 
     useEffect(() => {
-        controller && controller.handleMessage({
-            setStrokeColor: {
-                id: nodeId,
-                stroke_color: hexTorgba(strokeColor),
-            }
-        });
-    }, [controller, strokeColor, nodeId]);
+        controller &&
+            controller.handleMessage({
+                setStrokeColor: {
+                    id: nodeId,
+                    stroke_color: hexTorgba(strokeColor),
+                },
+            })
+    }, [controller, strokeColor, nodeId])
 
     return (
         <div>
             <h1>Color</h1>
             <div>
                 <label>Fill</label>
-                <input type="color" value={fillColor} onChange={(e) => {
-                    setFillColor(e.target.value);
-                }} />
+                <input
+                    type="color"
+                    value={fillColor}
+                    onChange={(e) => {
+                        setFillColor(e.target.value)
+                    }}
+                />
             </div>
             <div>
                 <label>Stroke</label>
-                <input type="color" value={strokeColor} onChange={(e) => {
-                    setStrokeColor(e.target.value);
-                }} />
+                <input
+                    type="color"
+                    value={strokeColor}
+                    onChange={(e) => {
+                        setStrokeColor(e.target.value)
+                    }}
+                />
             </div>
         </div>
-    );
+    )
 }
