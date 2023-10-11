46395ba9a8f93f7a66077eb9cc415ccbe60dc89a
Nikhil Gupta <reach.neoquest@gmail.com>
2023-10-11 17:54:56 +0530

    UI alignment for canvas and panels

--- apps/web/src/components/RadiantPropertiesPanel.tsx
+++ apps/web/src/components/RadiantPropertiesPanel.tsx
@@ -1,12 +1,12 @@
-import { RadiantColorPanel } from "./RadiantColorPanel";
-import { RadiantTransformPanel } from "./RadiantTransformPanel";
-import { Stack } from "@mui/material";
+import { RadiantColorPanel } from './RadiantColorPanel'
+import { RadiantTransformPanel } from './RadiantTransformPanel'
+import { Stack } from '@mui/material'
 
 export function RadiantPropertiesPanel() {
     return (
-        <Stack>
+        <Stack style={{ pointerEvents: 'all' }}>
             <RadiantTransformPanel />
             <RadiantColorPanel />
         </Stack>
-    );
+    )
 }
