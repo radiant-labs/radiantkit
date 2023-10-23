fc2b12bbf95e2b8eaca0a4e2144b9039d51f874e
Nikhil Gupta <reach.guptanikhil@gmail.com>
2023-10-23 18:06:29 +0530

    Fixes vercel build

--- apps/web/vercel_build.sh
+++ apps/web/vercel_build.sh
@@ -4,7 +4,7 @@ source "$HOME/.cargo/env"
 
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 
-~/.cargo/bin/wasm-pack build --target web
+cd ../../crates/wasm && ~/.cargo/bin/wasm-pack build --target web && cd -
 
 yarn install
 
