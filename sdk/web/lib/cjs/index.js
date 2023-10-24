e0d39ae56410a226d5003a2fcb30279465d4870c
Nikhil Gupta <reach.guptanikhil@gmail.com>
2023-10-24 16:37:59 +0530

    Adds doc generator and minor fix

--- sdk/web/lib/cjs/index.js
+++ sdk/web/lib/cjs/index.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RadiantCanvas = void 0;
 const tslib_1 = require("tslib");
-tslib_1.__exportStar(require("radiant-wasm"), exports);
 tslib_1.__exportStar(require("./context"), exports);
 tslib_1.__exportStar(require("./controller"), exports);
 const RadiantCanvas_1 = tslib_1.__importDefault(require("./components/RadiantCanvas"));
