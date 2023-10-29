a370254bb8d9ca4425afb0f6226a325e015a6b93
Nikhil Gupta <nikhil@getnyra.ai>
2023-10-29 14:13:28 +0530

    Renames examples and adds basic

--- crates/runtime/src/lib.rs
+++ crates/runtime/src/lib.rs
@@ -11,5 +11,9 @@ pub use radiant_image_node::RadiantImageNode;
 pub use radiant_path_node::RadiantPathNode;
 pub use radiant_text_node::RadiantTextNode;
 
+pub use radiant_winit::run_native;
+
 #[cfg(target_arch = "wasm32")]
 pub mod wasm;
+#[cfg(target_arch = "wasm32")]
+pub use radiant_winit::run_wasm;
