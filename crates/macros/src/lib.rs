9b861071af1e4420d4ed5d8018b5da022cdb0437
Nikhil Gupta <inbox.guptanikhil@gmail.com>
2023-11-03 17:02:42 +0530

    Adds macro for RadiantNode

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -53,9 +53,56 @@ fn derive_tessellatable_internal(item: TokenStream2) -> syn::Result<TokenStream2
     Ok(res)
 }
 
+fn derive_node_internal(item: TokenStream2) -> syn::Result<TokenStream2> {
+    let item = syn::parse2::<syn::ItemEnum>(item)?;
+
+    let name = item.ident.clone();
+    let node_names = item.variants.iter().map(|variant| {
+        variant.ident.clone()
+    }).collect::<Vec<_>>();
+
+    let res = quote! {
+        impl RadiantNode for #name {
+            fn get_id(&self) -> u64 {
+                match self {
+                    #(
+                        #name::#node_names(node) => node.get_id(),
+                    )*
+                }
+            }
+
+            fn set_id(&mut self, id: u64) {
+                match self {
+                    #(
+                        #name::#node_names(node) => node.set_id(id),
+                    )*
+                }
+            }
+
+            fn get_bounding_rect(&self) -> [f32; 4] {
+                match self {
+                    #(
+                        #name::#node_names(node) => node.get_bounding_rect(),
+                    )*
+                }
+            }
+        }
+    };
+    Ok(res)
+}
+
 #[proc_macro_derive(RadiantTessellatable)]
 pub fn derive_tessellatable(item: TokenStream) -> TokenStream {
     let res = match derive_tessellatable_internal(item.into()) {
+        Ok(res) => res,
+        Err(err) => err.to_compile_error(),
+    };
+    res.into()
+}
+
+#[proc_macro_derive(RadiantNode)]
+pub fn derive_node(item: TokenStream) -> TokenStream {
+    let res = match derive_node_internal(item.into()) {
         Ok(res) => res,
         Err(err) => err.to_compile_error(),
     };
