9f750d87a2e159854884cd9371b3cb20ee8dad39
Nikhil Gupta <nikhil@getnyra.ai>
2023-11-03 17:02:42 +0530

    Adds macro for RadiantNode

--- crates/runtime/src/node.rs
+++ crates/runtime/src/node.rs
@@ -5,13 +5,13 @@ use radiant_core::{
 };
 use radiant_core::{RadiantDocumentNode, RadiantGroupNode};
 use radiant_image_node::RadiantImageNode;
-use radiant_macros::RadiantTessellatable;
+use radiant_macros::{RadiantNode, RadiantTessellatable};
 use radiant_path_node::RadiantPathNode;
 use radiant_text_node::RadiantTextNode;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-#[derive(RadiantTessellatable)]
+#[derive(RadiantNode, RadiantTessellatable)]
 pub enum RadiantNodeType {
     Document(RadiantDocumentNode<RadiantNodeType>),
     Artboard(RadiantGroupNode<RadiantNodeType>),
@@ -19,41 +19,6 @@ pub enum RadiantNodeType {
     Path(RadiantPathNode),
     Image(RadiantImageNode),
     Text(RadiantTextNode),
-}
-
-impl RadiantNode for RadiantNodeType {
-    fn get_id(&self) -> u64 {
-        match self {
-            RadiantNodeType::Document(node) => node.get_id(),
-            RadiantNodeType::Artboard(node) => node.get_id(),
-            RadiantNodeType::Rectangle(node) => node.get_id(),
-            RadiantNodeType::Path(node) => node.get_id(),
-            RadiantNodeType::Image(node) => node.get_id(),
-            RadiantNodeType::Text(node) => node.get_id(),
-        }
-    }
-
-    fn set_id(&mut self, id: u64) {
-        match self {
-            RadiantNodeType::Document(node) => node.set_id(id),
-            RadiantNodeType::Artboard(node) => node.set_id(id),
-            RadiantNodeType::Rectangle(node) => node.set_id(id),
-            RadiantNodeType::Path(node) => node.set_id(id),
-            RadiantNodeType::Image(node) => node.set_id(id),
-            RadiantNodeType::Text(node) => node.set_id(id),
-        }
-    }
-
-    fn get_bounding_rect(&self) -> [f32; 4] {
-        match self {
-            RadiantNodeType::Document(node) => node.get_bounding_rect(),
-            RadiantNodeType::Artboard(node) => node.get_bounding_rect(),
-            RadiantNodeType::Rectangle(node) => node.get_bounding_rect(),
-            RadiantNodeType::Path(node) => node.get_bounding_rect(),
-            RadiantNodeType::Image(node) => node.get_bounding_rect(),
-            RadiantNodeType::Text(node) => node.get_bounding_rect(),
-        }
-    }
 }
 
 impl RadiantComponentProvider for RadiantNodeType {
