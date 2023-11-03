25464b342a3086db28b378c2120349d4905e95b3
Nikhil Gupta <reach.guptanikhil@gmail.com>
2023-11-03 17:05:28 +0530

    Adds macro for RadiantComponentProvider

--- crates/runtime/src/node.rs
+++ crates/runtime/src/node.rs
@@ -5,13 +5,13 @@ use radiant_core::{
 };
 use radiant_core::{RadiantDocumentNode, RadiantGroupNode};
 use radiant_image_node::RadiantImageNode;
-use radiant_macros::{RadiantNode, RadiantTessellatable};
+use radiant_macros::{RadiantComponentProvider, RadiantNode, RadiantTessellatable};
 use radiant_path_node::RadiantPathNode;
 use radiant_text_node::RadiantTextNode;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-#[derive(RadiantNode, RadiantTessellatable)]
+#[derive(RadiantComponentProvider, RadiantNode, RadiantTessellatable)]
 pub enum RadiantNodeType {
     Document(RadiantDocumentNode<RadiantNodeType>),
     Artboard(RadiantGroupNode<RadiantNodeType>),
@@ -19,28 +19,4 @@ pub enum RadiantNodeType {
     Path(RadiantPathNode),
     Image(RadiantImageNode),
     Text(RadiantTextNode),
-}
-
-impl RadiantComponentProvider for RadiantNodeType {
-    fn get_component<T: RadiantComponent + 'static>(&self) -> Option<&T> {
-        match self {
-            RadiantNodeType::Document(node) => node.get_component(),
-            RadiantNodeType::Artboard(node) => node.get_component(),
-            RadiantNodeType::Rectangle(node) => node.get_component(),
-            RadiantNodeType::Path(node) => node.get_component(),
-            RadiantNodeType::Image(node) => node.get_component(),
-            RadiantNodeType::Text(node) => node.get_component(),
-        }
-    }
-
-    fn get_component_mut<T: RadiantComponent + 'static>(&mut self) -> Option<&mut T> {
-        match self {
-            RadiantNodeType::Document(node) => node.get_component_mut(),
-            RadiantNodeType::Artboard(node) => node.get_component_mut(),
-            RadiantNodeType::Rectangle(node) => node.get_component_mut(),
-            RadiantNodeType::Path(node) => node.get_component_mut(),
-            RadiantNodeType::Image(node) => node.get_component_mut(),
-            RadiantNodeType::Text(node) => node.get_component_mut(),
-        }
-    }
 }
