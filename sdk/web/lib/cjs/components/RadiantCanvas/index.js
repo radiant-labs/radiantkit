bd9d2d5f9284d4412757b1a9bcb7f8ce677a1382
Nikhil Gupta <inbox.guptanikhil@gmail.com>
2023-11-05 18:38:11 +0530

    Cleans up web example

--- sdk/web/lib/cjs/components/RadiantCanvas/index.js
+++ sdk/web/lib/cjs/components/RadiantCanvas/index.js
@@ -4,15 +4,7 @@ exports.RadiantCanvas = void 0;
 const tslib_1 = require("tslib");
 const react_1 = tslib_1.__importDefault(require("react"));
 const RadiantCanvas = () => {
-    return (react_1.default.createElement("div", { id: "canvas-container", style: {
-            position: 'absolute',
-            zIndex: 0,
-            display: 'flex',
-            alignItems: 'center',
-            height: '100%',
-            justifyContent: 'center',
-            width: '100%',
-        } }));
+    return (react_1.default.createElement("div", { id: "canvas-container" }));
 };
 exports.RadiantCanvas = RadiantCanvas;
 //# sourceMappingURL=index.js.map
