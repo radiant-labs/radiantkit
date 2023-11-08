2f200f9f71bfa644c673aaa2b6d167f65c2f9a29
Nikhil Gupta <inbox.guptanikhil@gmail.com>
2023-11-08 17:22:57 +0530

    Removes lp

--- lp/README.md
+++
@@ -1,1 +0,0 @@
-# Radiant Landing Page
--- lp/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: '/radiant',
-}
-
-module.exports = nextConfig
