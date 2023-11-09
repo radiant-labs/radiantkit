4fbf720a05fc99317e454728474a7a9861ab0a6e
Nikhil Gupta <inbox.guptanikhil@gmail.com>
2023-11-09 12:54:57 +0530

    Updates publish script

--- runtime/publish.sh
+++ runtime/publish.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 set -ex
-cd ../crates
-cd core
-cargo publish
-cd ..
-cd macros
-cargo publish
-cd ..
-cd image
-cargo publish
-cd ..
-cd text
-cargo publish
-cd ..
-cd path
-cargo publish
-cd ..
-cd winit
-cargo publish
-cd ..
+# cd ../crates
+# cd core
+# cargo publish
+# cd ..
+# cd macros
+# cargo publish
+# cd ..
+# cd image
+# cargo publish
+# cd ..
+# cd text
+# cargo publish
+# cd ..
+# cd path
+# cargo publish
+# cd ..
+# cd winit
+# cargo publish
+# cd ..
+# cd ../runtime
 cargo publish
 echo "published successfully."
