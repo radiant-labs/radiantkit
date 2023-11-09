23e3cfa6268806a187b051f461f44a7789ce98eb
Nikhil Gupta <inbox.guptanikhil@gmail.com>
2023-11-09 11:09:57 +0530

    Warn for broken doc links

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'radiant-labs', // Usually your GitHub org/user name.
   projectName: 'radiantkit', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
