68b2242c61b3a41c62dec65ffc796b8d053ddf69
Nikhil Gupta <nikhil@getnyra.ai>
2023-11-13 15:16:23 +0530

    Tries to fix adapter issue

--- crates/winit/src/lib.rs
+++ crates/winit/src/lib.rs
@@ -79,7 +79,7 @@ impl<M: From<RadiantSceneMessage> + TryInto<RadiantSceneMessage>, N: RadiantNode
                     // WebGL doesn't support all of wgpu's features, so if
                     // we're building for the web we'll have to disable some.
                     limits: if cfg!(target_arch = "wasm32") {
-                        wgpu::Limits::downlevel_webgl2_defaults()
+                        wgpu::Limits::downlevel_webgl2_defaults().using_resolution(adapter.limits())
                     } else {
                         wgpu::Limits::default()
                     },
