16d06b2551230bd52100acb00534d7bb9bba69bd
Nikhil Gupta <nikhil@getnyra.ai>
2023-11-17 19:03:14 +0530

    Minor update

--- runtime/src/tools.rs
+++
@@ -1,9 +0,0 @@
-use radiantkit_core::{SelectionTool, RectangleTool};
-
-use radiantkit_macros::radiant_wasm_bindgen;
-
-#[radiant_wasm_bindgen]
-pub struct Tools {
-    pub selection: SelectionTool,
-    pub rectangle: RectangleTool,
-}
