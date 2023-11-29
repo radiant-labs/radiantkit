faa25724efda916905284450f577ad073a2cb5c9
Nikhil Gupta <inbox.guptanikhil@gmail.com>
2023-11-29 10:23:27 +0530

    Adds ability to modify text

--- crates/text/src/text_message.rs
+++ crates/text/src/text_message.rs
@@ -3,6 +3,19 @@ use serde::{Deserialize, Serialize};
 
 #[export_tokens]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub enum RadiantTextMessage {
+pub enum RadiantAddTextMessage {
     AddText { text: String, position: [f32; 2] },
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum RadiantTextMessage {
+    SetText { id: u64, text: String },
+}
+
+impl RadiantTextMessage {
+    pub fn id(&self) -> u64 {
+        match self {
+            RadiantTextMessage::SetText { id, .. } => *id,
+        }
+    }
 }
