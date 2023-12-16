c7b0ce2d4b330527ee8571e3ce254f4c3edff9c0
Nikhil Gupta <nikhil@getnyra.ai>
2023-12-16 19:32:14 +0530

    Fixes message send from wasm

--- runtime/pkg/radiantkit.d.ts
+++ runtime/pkg/radiantkit.d.ts
@@ -207,8 +207,8 @@ export interface InitOutput {
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h20324fb4a9880f17: (a: number, b: number) => void;
-  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0077550e1d4ebdc0: (a: number, b: number, c: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke0_mut__h0b630077be010e86: (a: number, b: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke1_mut__h00644ecee464bd99: (a: number, b: number, c: number) => void;
   readonly wasm_bindgen__convert__closures__invoke1_mut__hb42503db276de2ca: (a: number, b: number, c: number) => void;
   readonly wasm_bindgen__convert__closures__invoke0_mut__h259ebfa3249daa59: (a: number, b: number) => void;
   readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5234d6fbfa4fe934: (a: number, b: number, c: number) => void;
